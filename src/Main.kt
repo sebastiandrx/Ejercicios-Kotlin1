67cd38b5e21dac6e8782b2b40d84d60434f82afd
sebastiandrx <sebastianduenas05@gmail.com>
2025-02-14 14:05:53 -0500

    Operaciones Basicas

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,14 @@ fun main(){
     println("Hola Mundo")
     println("Sebastian Dueñas R")
     println("Jugar futbol")
+
+
+
+
+    println("Estoy en mi rama de Desarrollo")
+    println("SUMA")
+    println("RESTA")
+    println("MULTIPLICACION")
+    println("DIVISION")
+
 }
