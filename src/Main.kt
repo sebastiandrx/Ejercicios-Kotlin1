08145dea5318c843f94d368f0fb72b475b6db6bf
sebastiandrx <sebastianduenas05@gmail.com>
2025-02-14 13:48:16 -0500

    Ejercicio de conocer

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,6 @@
 fun main(){
 
     println("Hola Mundo")
-    println("Primer ejercicio Kotlin")
-    println("Primer ejercicio Kotlin")
-    println("Primer ejercicio Kotlin")
-    println("Primer ejercicio Kotlin")
-    println("Primer ejercicio Kotlin")
+    println("Sebastian Dueñas R")
+    println("Jugar futbol")
 }
